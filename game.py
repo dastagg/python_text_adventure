8be53d985b714674f47dacc0a8c2321ab6d2b18d
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-08 21:31:27 -0500

    Commit before DSL section

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from player import Player
 import world
 
@@ -14,26 +15,47 @@ def play():
         room = world.tile_at(player.x, player.y)
         print(room.intro_text())
         room.modify_player(player)
-        action_input = get_player_command()
-        action_input = action_input.lower()
-        if action_input == "n":
-            player.move_north()
-        elif action_input == "s":
-            player.move_south()
-        elif action_input == "e":
-            player.move_east()
-        elif action_input == "w":
-            player.move_west()
-        elif action_input == "i":
-            player.print_inventory()
-        elif action_input == "h":
-            player.heal()
+        choose_action(room, player)
+
+
+def choose_action(room, player):
+    action = None
+    while not action:
+        available_actions = get_available_actions(room, player)
+        action_input = input("Action: ")
+        action = available_actions.get(action_input)
+        if action:
+            action()
         else:
             print("Invalid action!")
 
 
-def get_player_command():
-    return input("Action: ")
+def get_available_actions(room, player):
+    actions = OrderedDict()
+    print("Choose an action: ")
+    if player.inventory:
+        action_adder(actions, "i", player.print_inventory, "Print inventory")
+    if isinstance(room, world.EnemyTile) and room.enemy.is_alive():
+        action_adder(actions, "a", player.attack, "Attack")
+    else:
+        if world.tile_at(room.x, room.y - 1):
+            action_adder(actions, "n", player.move_north, "Go north")
+        if world.tile_at(room.x, room.y + 1):
+            action_adder(actions, "s", player.move_south, "Go south")
+        if world.tile_at(room.x + 1, room.y):
+            action_adder(actions, "e", player.move_east, "Go east")
+        if world.tile_at(room.x - 1, room.y):
+            action_adder(actions, "w", player.move_west, "Go west")
+    if player.hp < 100:
+        action_adder(actions, "h", player.heal, "Heal")
+
+    return actions
+
+
+def action_adder(action_dict, hotkey, action, name):
+    action_dict[hotkey.lower()] = action
+    action_dict[hotkey.upper()] = action
+    print(f"{hotkey}: {name}")
 
 
 play()
