d929aac6ee2f98be6bbe6222b2008abfaafa3154
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-09 07:07:41 -0500

    Finish Chapter 15

--- player.py
+++ player.py
@@ -11,6 +11,10 @@ class Player(object):
         self.y = world.start_tile_location[1]
         self.hp = 100
         self.gold = 5
+        self.victory = False
+
+    def is_alive(self):
+        return self.hp > 0
 
     def move(self, dx, dy):
         self.x += dx
